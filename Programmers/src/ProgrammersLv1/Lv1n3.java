8e705d77ae4cd05fd4a29426ea9fe1d4a5505351
cj <ckswls1019@naver.com>
2024-09-18 21:10:23 +0900

    프로그래머스 평균 구하기

---
+++ Programmers/src/ProgrammersLv1/Lv1n3.java
@@ -0,0 +1,15 @@
+package ProgrammersLv1;
+
+//평균 구하기
+public class Lv1n3 {
+    public double solution(int[] numbers) {
+        double answer = 0;
+        
+        for(int num:numbers){
+            answer += num;
+        }
+        return answer/numbers.length;
+    }
+} {
+    
+}
