56999cb6efe0c6a495cfa70af63ceb4cab5dd4d9
cj <ckswls1019@naver.com>
2024-09-25 23:41:16 +0900

    프로그래머스 옹알이

---
+++ Programmers/src/ProgrammersLv1/Lv1n4.java
@@ -0,0 +1,26 @@
+package ProgrammersLv1;
+
+class Lv1n4 {
+    public int solution(String[] babbling) {
+        int result = 0; 
+        String[] validWords = {"aya", "ye", "woo", "ma"};  
+        
+       
+        for (String babble : babbling) {
+           
+            for (String word : validWords) {
+               
+                if (babble.indexOf(word) > -1) {
+                    // 유효한 단어를 빈칸으로 대체
+                    babble = babble.replace(word, " ");
+                }
+            }
+         
+            if (babble.trim().length() == 0) {
+                result++;  
+            }
+        }
+        
+        return result;  
+    }
+}
