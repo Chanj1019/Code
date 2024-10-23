ebd693d98d8b3ca73a0ea3ca2d0605d6f62f7df9
cj <ckswls1019@naver.com>
2024-10-23 20:27:23 +0900

    6번째 문제완료

---
+++ Programmers/src/ProgrammersLv1/Lv1n6.java
@@ -0,0 +1,17 @@
+
+public class Lv1n6 {
+    public int solution(int n) {
+        int result = 0;
+        
+   
+        String str = String.valueOf(n);
+        
+     
+        for (int i = 0; i < str.length(); i++) {
+           
+            result += Integer.valueOf(str.charAt(i) - '0');
+        }
+        
+        return result;
+    }
+}
